cb42df4295e7b3be804178457acf4395a58990b5
Victor Grabowski <victor.grabowski@delfox.net>
2023-12-12 11:38:52 +0100

    project euler init

---
+++ problem2.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def get_even_fibonacci_number_under(max):
+    total = []
+    a = 1
+    b = 2
+    while a < max:
+        pass
+        # WIP
+        # for i in range(0, 4000000):
+        #     for multiplier in max:
+        #         if i % multiplier == 0:
+        #             total.append(i)
+        # print(sum(set(total)))
+        # return total
+    # Use a breakpoint in the code line below to debug your script.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_even_fibonacci_number_under(4000000)
+
